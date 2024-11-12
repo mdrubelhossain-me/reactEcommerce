06635f775641761f08294e0b1d0922576f2588e6
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2024-11-13 01:25:56 +0600

    hero section added

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Hero.css";
-import bg from "../../assets/images/hero.jpg";
+import bg from "../../assets/images/hero3.jpg";
+import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <div>
@@ -8,7 +9,13 @@ const Hero = () => {
         <div className="over-lay"></div>
         <img src={bg} alt="" />
         <div className="content">
-          <div className="container"></div>
+          <div className="text-center">
+            <h6>New Arrivals Only</h6>
+            <h1>Fresh & Organic Fruits <br /> <b>and</b> <br /> Vegetables for a Healthier You</h1>
+            <div>
+                <Link className="myBtn">See More <i className="bi bi-arrow-right"></i></Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
